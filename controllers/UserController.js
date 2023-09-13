0c5fd1823fbb5f5c13bbff6c67e17d7aa121438c
José-Capucho <joseantoniocapucho@gmail.com>
2023-09-13 16:43:30 -0300

    Subindo alterações do projeto

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -8,6 +8,7 @@ class UserController {
 
         this.onSubmit();
         this.onEdit();
+        this.selectAll();
 
     }
 
@@ -96,6 +97,8 @@ class UserController {
             this.getPhoto(this.formEl).then((content) => {
 
                 values.photo = content;
+
+                this.insert(values);
 
                 this.addLine(values);
 
@@ -197,6 +200,46 @@ class UserController {
             user.photo, 
             user.admin
         );
+
+    }
+
+    getUsersStorage(){
+
+        let users = [];
+
+        if (sessionStorage.getItem("users")) {
+
+            users = JSON.parse(sessionStorage.getItem("users"));
+
+        }
+
+        return users;
+
+    }
+
+    selectAll(){
+
+        let users = this.getUsersStorage();
+
+        users.forEach(dataUser => {
+
+            let user = new User();
+
+            user.loadFromJSON(dataUser);
+
+            this.addLine(user);
+
+        });
+
+    }
+
+    insert(data){
+
+        let users = this.getUsersStorage();
+
+        users.push(data);
+
+        sessionStorage.setItem("users", JSON.stringify(users));
 
     }
 
